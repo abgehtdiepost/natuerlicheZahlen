4a9ffd2d11e286d01426c57656fc1f4d9d93e90f
abgehtdiepost <david.gruber@rb-werbeagentur.at>
2019-09-23 22:20:02 +0200

    sums

--- Numbers.java
+++ Numbers.java
@@ -5,12 +5,15 @@
  */
 package hue1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author david
  */
 public class Numbers {
-    int limit;
+    public int limit;
+    public ArrayList<Integer> sums;
     
     public Numbers (int limit) {
         if (checkLimit(limit)) {
@@ -19,13 +22,31 @@ public class Numbers {
             System.out.println("Bitte eine ordnugnsgemäße Grenze angeben!");
             System.exit(0);
         }
+        sums = new ArrayList<>();
+        getSums();
+        printDone();
         
     }
     
     public boolean checkLimit (int limit) {
+        if (limit == 2 || limit == 0) {
+            return false;
+        }
         if (limit % 2 == 0) {
             return true;
         }
         return false;
+    }
+    
+    public void getSums () {
+        for (int i = 4; i <= limit; i+=2) {
+            sums.add(i);
+        }
+    }
+    
+    public void printDone () {
+        for (int i = 0; i < sums.size(); i++) {
+            System.out.println(sums.get(i) + " summe: ");
+        }
     }
 }
